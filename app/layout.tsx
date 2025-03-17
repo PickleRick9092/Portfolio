76f9af1e8e3f095572e2ed9e6fd7a3877d9aa16a
Mohammad <Mr.math9092@gmail.com>
2025-03-17 11:34:47 +0330

    Installing Search Console using meta V01

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,14 @@ export const metadata: Metadata = {
   title: 'Mohammad Kalateh | Web Designer & SEO Expert',
   description: 'I am Mohammad Kalateh 😊, a Web Designer & SEO Expert 🚀. I help businesses grow and get noticed online 🌐 through stunning websites and smart SEO strategies',
 };
+export const metadatas = {
+  title: "Your Website Title",
+  description: "Your website description",
+  other: {
+    "google-site-verification": "Z_NpTA0NCoh4XwtdPm_j5G_PaTtVr9-HX2ocqHKdSic",
+  },
+};
+
 
 export default function RootLayout({
   children,
