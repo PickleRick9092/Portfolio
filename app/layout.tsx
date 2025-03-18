d7761f59badfa995f8db6d4886aaa70305571435
Mohammad <Mr.math9092@gmail.com>
2025-03-19 01:13:48 +0330

    Adding Person & Website Schema V01

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from "next/head";
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -15,15 +16,51 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const personSchema = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "name": "Mohammad Kalateh",
+    "url": "https://mohammadkalateh.com/",
+    "sameAs": [
+      "https://www.linkedin.com/in/mohammad-kalateh-46b65a239/",
+      "https://github.com/PickleRick9092"
+    ],
+    "jobTitle": "SEO Expert & Web Designer",
+    "worksFor": {
+      "@type": "Organization",
+      "name": "Mohammad Kalateh"
+    }
+  };
+
+  const websiteSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "Mohammad Kalateh",
+    "url": "https://mohammadkalateh.com/",
+  };
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(personSchema),
+          }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(websiteSchema),
+          }}
+        />
+      </Head>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Analytics />
